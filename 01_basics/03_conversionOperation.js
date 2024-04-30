b7083549f5a83042ce1a76aebbff8ed39287c8c9
Rifaqatsumbal <34680154+Rifaqatsumbal@users.noreply.github.com>
2024-04-30 07:11:48 +0000

    datatype summery

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,27 @@
-let score = 33
-console.log(typeof score);
-console.log(typeof(score))
+/*  try by uncommenting
+let score = 33 // value in already in number
+console.log(typeof score); // will give type of the variable "score"
+console.log(typeof(score)); // will give type of the variable "score"
+*/
+
+/* try by uncommenting
+let score = "33"  // value in string
+console.log(typeof score); // type is string
+let valueInNumber = Number(score); // this will convert string into number
+console.log(typeof valueInNumber); // it will give type number as converted above
+console.log(valueInNumber); // this will give value stored in variable valueInNumber */
+
+
+// Note: it will convert string into number even if score = "33abc", but when you print its value it will 
+// give you NAN
+
+// Summery try below value it will give you follow resut after conversion
+// if "33" = 33, 
+// "33abc" = NAN, 
+// null = 0
+// true = 1, false = 0
+
+// lets try boolean conversion
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // this will give you true value inverse for 0
---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,24 @@
+
+// Primitive
+// Primitive are 7 Types: String, number, bolean, null, undefined, symbol, BigInt
+
+// symbol use for uniquness like to make a thing unique like id
+// Example
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid); // it will give me false 
+
+// Refrence (Non Primitive)
+// Refrence are Arrays, Objects, functions
+const cities = [Karachi, Isalamabad, Peshawer, Quetta] // called array
+
+let myObj{
+    name = "Rifa",
+    age = 23,
+    gender = "male"
+} // this is object stored in a variable myObj
+
+const myFunction = function(){} // this is basics syntex of function, 
+ 
+
+
